7747741c6fe23e5f9badda8a98d70eb6f82c1699
Andrus Naulainen <andrus.naulainen@gmail.com>
2020-09-25 11:51:38 +0300

    Test winning conditions

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ cellDivs.forEach(el => {
             selectedCell.innerText = 'X'
 
             gameBoard[selectedRow][selectedCol] = 'X'
+
+            if ( gameBoard[0][0] == 'X' && gameBoard[0][1] == 'X' && gameBoard[0][2] == 'X' ) {
+                console.log('player win')
+            }
+
+            if ( gameBoard[1][0] == 'X' && gameBoard[1][1] == 'X' && gameBoard[1][2] == 'X' ) {
+                console.log('player win')
+            }
+
+            if ( gameBoard[2][0] == 'X' && gameBoard[2][1] == 'X' && gameBoard[2][2] == 'X' ) {
+                console.log('player win')
+            }
     
             console.log(gameBoard)
         }
